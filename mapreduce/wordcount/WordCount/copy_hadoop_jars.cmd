4e54ca4914fdf61783d1ec74ae30bc2d869d93c0
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2022-11-23 21:02:53 +0100

    bug fixed

--- mapreduce/wordcount/WordCount/copy_hadoop_jars.cmd
+++ mapreduce/wordcount/WordCount/copy_hadoop_jars.cmd
@@ -1,4 +1,4 @@
 @echo off
-
+mkdir lib
 docker "cp" "hadoop:\hadoop-2.10.1\share\hadoop\common\hadoop-common-2.10.1.jar" "lib/"
 docker "cp" "hadoop:\hadoop-2.10.1\share\hadoop\mapreduce\hadoop-mapreduce-client-core-2.10.1.jar" "lib/"
