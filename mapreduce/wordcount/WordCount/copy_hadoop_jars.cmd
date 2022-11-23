ceac0c552f840b0a0d2eda6fb4100a3b8906cdfd
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2022-11-23 20:59:09 +0100

    added win copy script

---
+++ mapreduce/wordcount/WordCount/copy_hadoop_jars.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+docker "cp" "hadoop:\hadoop-2.10.1\share\hadoop\common\hadoop-common-2.10.1.jar" "lib/"
+docker "cp" "hadoop:\hadoop-2.10.1\share\hadoop\mapreduce\hadoop-mapreduce-client-core-2.10.1.jar" "lib/"
