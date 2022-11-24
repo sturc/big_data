0c31beaca13ab5e899bd501b4a5ae6693cb7d741
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2022-11-24 11:52:06 +0100

    map reduce deployment for azure lab added

--- mapreduce/wordcount/WordCount/copy_hadoop_jars.sh
+++ mapreduce/wordcount/WordCount/copy_hadoop_jars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker cp hadoop:/hadoop-2.10.1/share/hadoop/common/hadoop-common-2.10.1.jar lib/
+docker cp hadoop:/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-client-common-2.10.1.jar lib/
 docker cp hadoop:/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.10.1.jar lib/
